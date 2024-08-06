ad1c70b1a53b99fb5e3c12aae3768dcfa0f06022
한상훈 <hzyan2@gmail.com>
2024-08-06 13:03:58 +0900

    kafak port 변경

--- src/main/java/lol/mmrtr/lolrepository/config/KafkaConsumerConfig.java
+++ src/main/java/lol/mmrtr/lolrepository/config/KafkaConsumerConfig.java
@@ -21,7 +21,7 @@ public class KafkaConsumerConfig {
     public ConsumerFactory<String, Object> consumerFactory() {
 
         Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9094");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_1");
 
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
