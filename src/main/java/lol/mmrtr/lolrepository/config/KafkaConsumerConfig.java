44592c6d331082336896c61e602fe9856726349e
한상훈 <hzyan2@gmail.com>
2024-08-08 09:50:00 +0900

    properties 수저정

--- src/main/java/lol/mmrtr/lolrepository/config/KafkaConsumerConfig.java
+++ src/main/java/lol/mmrtr/lolrepository/config/KafkaConsumerConfig.java
@@ -2,6 +2,7 @@ package lol.mmrtr.lolrepository.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -16,11 +17,14 @@ import java.util.Map;
 @Configuration
 public class KafkaConsumerConfig {
 
+    @Value("${spring.kafka.consumer.bootstrap-servers}")
+    private String boostrapServers;
+
     @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
 
         Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9094");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServers);
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_1");
 
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
